1d58e813a9af3410f48f7159ec9b3d9d1afbe62f
Nirmal Patel <patelnirmal1325@gmail.com>
2020-12-14 17:27:06 -0500

    added adding.js

---
+++ javascript/adding.js
@@ -0,0 +1,9 @@
+//Add the first and last name of a variable
+var firstName = "Nirmal";
+var lastName = "Patel";
+var fullName = firstName + " " + lastName;
+
+//Add two numbers and store them in a variable
+var a = 34;
+var b = 21;
+var c = a + b; 
