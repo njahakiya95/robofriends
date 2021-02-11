1bbdd1e56604418398375ee63273a49cde211cde
Nirmal Patel <patelnirmal1325@gmail.com>
2021-02-11 14:52:39 -0500

    updated App and index.js

--- javascript/react/robofriends/src/App.js
+++ javascript/react/robofriends/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList'; 
 import { robots } from './robots'; 
 import SearchBox from './SearchBox'; 
 
-const App = () => {
-	return (
-		<div className = 'tc'>
-			<h1>Robofriends</h1>
-			<SearchBox />
-			<CardList robots = {robots}/>
-		</div>
-	); 
+const state = {
+	robots: robots,
+	searchfield: ''
 }
+
+class App extends Component {
+	render() {
+		return (
+			<div className = 'tc'>
+				<h1>Robofriends</h1>
+				<SearchBox />
+				<CardList robots = {robots}/>
+			</div>
+		);
+	}
+}
+
 export default App;
