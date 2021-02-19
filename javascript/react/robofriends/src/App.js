b267abab3ffb4bf9c765f53579b79f3e4e84682a
Nirmal Patel <patelnirmal1325@gmail.com>
2021-02-19 00:26:31 -0500

    modified robofriends files

--- javascript/react/robofriends/src/App.js
+++ javascript/react/robofriends/src/App.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import CardList from './CardList'; 
 import { robots } from './robots'; 
 import SearchBox from './SearchBox'; 
-
-const state = {
-	robots: robots,
-	searchfield: ''
-}
+import './App.css';
 
 class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+	}
+
 	render() {
+		const filteredRobots = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()); 
+		})
 		return (
 			<div className = 'tc'>
-				<h1>Robofriends</h1>
-				<SearchBox />
-				<CardList robots = {robots}/>
+				<h1 className= 'f1'>Robofriends</h1>
+				<SearchBox searchChange = {this.onSearchChange}/>
+				<CardList robots = {filteredRobots}/>
 			</div>
 		);
 	}
