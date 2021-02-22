3fd2b318c0786b10e9f1a511989c6f5e2e36c034
Nirmal Patel <patelnirmal1325@gmail.com>
2021-02-22 18:05:10 -0500

    completed robofriends

--- javascript/react/robofriends/src/containers/App.js
+++ javascript/react/robofriends/src/containers/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import CardList from './CardList'; 
-import { robots } from './robots'; 
-import SearchBox from './SearchBox'; 
+import CardList from '../components/CardList'; 
+import SearchBox from '../components/SearchBox'; 
 import './App.css';
+import Scroll from '../components/Scroll'; 
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
 	}
 	
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({robots: users}));
+	}
+
 	onSearchChange = (event) => {
 		this.setState({ searchfield: event.target.value })
 	}
@@ -25,7 +31,9 @@ class App extends Component {
 			<div className = 'tc'>
 				<h1 className= 'f1'>Robofriends</h1>
 				<SearchBox searchChange = {this.onSearchChange}/>
-				<CardList robots = {filteredRobots}/>
+				<Scroll>
+					<CardList robots = {filteredRobots}/>
+				</Scroll>
 			</div>
 		);
 	}
