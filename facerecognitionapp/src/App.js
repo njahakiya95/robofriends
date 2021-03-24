f15d3f52b1bd02fbeaa8945c1da0e81ad83f2182
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-24 13:35:49 -0400

    modified facerecogapp files

--- facerecognitionapp/src/App.js
+++ facerecognitionapp/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js'; 
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';  
 import Navigation from './components/Navigation/Navigation'; 
 import Logo from './components/Logo/Logo'; 
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css'; 
+
+const app = new Clarifai.App({
+  apiKey: 'b54a0fc683434fa7a325e4b8de5775a8', 
+});
 
 const particlesOptions = {
   particles: {
@@ -19,6 +24,30 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict({id:'MODEL_ID', version:'MODEL_VERSION_ID'}, "https://samples.clarifai.com/metro-north.jpg").then(
+      function(response) {
+    // do something with response
+      },
+      function(err) {
+    // there was an error
+  }
+);
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,7 +57,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
       </div>
     );
   }
