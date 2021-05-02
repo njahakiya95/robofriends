3cf87b66809e7495253d7f8c33177129fee2134a
Nirmal Patel <patelnirmal1325@gmail.com>
2021-05-01 23:51:45 -0400

    updated smart-brain project

--- facerecognitionapp/src/App.js
+++ facerecognitionapp/src/App.js
@@ -34,8 +34,25 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',        
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -65,19 +82,33 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            }) 
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user,{ entries: count}))
+            })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
-onRouteChange = (route) => {
-  if (route === 'signout') {
-    this.setState({isSignedIn: false}) 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false}) 
   } else if (route === 'home') {
-    this.setState({isSignedIn: true})
+      this.setState({isSignedIn: true})
   }
-  this.setState({route: route}); 
-}
-
+    this.setState({route: route}); 
+  }
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
@@ -90,17 +121,20 @@ onRouteChange = (route) => {
         { route === 'home'
           ? <div>
               <Logo />
-              <Rank />
+              <Rank
+                name={this.state.user.name}
+                entries={this.state.user.entries} 
+              />
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit}
               />
-              <FaceRecognition box={this.state.box} imageUrl={imageUrl} />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
           : (
               route === 'signin' 
-              ? <SignIn onRouteChange={this.onRouteChange}/>
-              : <Register onRouteChange={this.onRouteChange}/>
+              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
         }
       </div>
